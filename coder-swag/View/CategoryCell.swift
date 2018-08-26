13228e15b236ad1d59be14b8d46986a0352f0da2
Anthony Hall <anthonyhall@anthonys-imac.local>
2018-08-26 14:04:35 -0700

    Initial Commit

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Anthony Hall on 8/26/18.
+//  Copyright © 2018 Anthony Hall. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
